71363d70ea30f263ef180ceed48f767e97fcd92d
Lynx1414 <calabrin.delphine@orange.fr>
2023-01-26 11:03:40 +0100

    height for aside changed and script initialized

--- script.js
+++ script.js
@@ -1,30 +1,41 @@
-let database = []
+const availableProducts = [{
+  ref: 23112022,
+  imgSrc: "./assets/screwdriver-electricity.png",
+  imgAlt: "electric screwdriver",
+  price: 15.00
+},
+{
+  ref: 24112022,
+  imgSrc: "./assets/screwdriver-torque.png",
+  imgAlt: "screwdriver torque",
+  price: 12.00
+},
+  /*faire idem pour tous les autres articles.
+  const availableProducts est le nom de notre tableau qui contient des objets (c'est à dire nos articles).
+  La syntaxe est des [] pour dire tableau et des {} pour dire objets et des propriétés suivies de : et enfin des valeurs (comme pour les variables). 
+  Faire attention aux virgules à la fin du couple propriété: valeur, et à la fin des {},*/
 
 
-/*
-function add() {
-    for (let i = 0; i < database.length; i++) {
-        let divContainer = document.createElement("div");
-        let divContenu = document.createElement("div");
-        let divArticle = document.createElement("div");
-        let image = document.createElement("img");
-        let span = document.createElement("span");
-        let ref;
-        span.textContent = ref ;
+];
 
-        let span1 = document.createElement("span");
-        let inputQuantite = document.createElement("input");
 
-        let prix = document.createElement("span");
-        prix.textContent = "9.99€"
-        let poubelle = document.createElement("button");
 
-        divArticle.append(image, span, span1, inputQuantite);
-        divContenu.append(divArticle, prix, poubelle);
-        divContainer.append(divContenu)
+let SectionElement = document.querySelector("section");// Element parent des div outils
 
-        let aside = document.querySelector("aside")
-        aside.append(divContainer)
-    }
+function displayAvailableProducts() {
+  for (let i = 0; i < availableProducts.length; i++) {
+    let article = `
+    <div class="outils">
+    <h4>Ref: ${availableProducts[i].ref}</h4>
+    <img src= ${availableProducts[i].imgSrc} alt= ${availableProducts[i].imgAlt}>
+    <p class="price">${availableProducts[i].price}€</p>
+    <label>Qté: </label>
+    <input class="quantite" type="number" min="1" max="99" value="1"><br>
+    <button class="addButton">ajouter</button>
+    </div>
+    `// est une autre méthode pour createElement
+
+    SectionElement.append(article);// ajouter les outils à l'élément parent
+  }
 }
-*/
+// displayAvailableProducts ()
